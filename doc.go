22118aa4f600e1b477ecb102260dadd207464ebb
Jean-François Bustarret <jf@bustarret.com>
2018-07-02 18:33:07 +0200

    Add units (#1)

--- doc.go
+++ doc.go
@@ -1,2 +1,2 @@
-// The senml package implements the Sensor Markup Language, as defined by https://tools.ietf.org/html/draft-ietf-core-senml-16.
+// Package senml implements the Sensor Markup Language, as defined by https://tools.ietf.org/html/draft-ietf-core-senml-16.
 package senml
