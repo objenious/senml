6b75d4a04fc3fcfccc834847f600f5fc0b22d031
jfbus <jf@bustarret.com>
2018-07-05 09:11:21 +0200

    add Float/Bool/True/False helpers

---
+++ ptr.go
@@ -0,0 +1,16 @@
+package senml
+
+var (
+	True  = Bool(true)
+	False = Bool(false)
+)
+
+// Bool returns a pointer to a bool value, to be used for Record.BoolValue.
+func Bool(b bool) *bool {
+	return &b
+}
+
+// Float returns a pointer to a float64 value, to be used for Record.Value.
+func Float(f float64) *float64 {
+	return &f
+}
