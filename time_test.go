dfc46cb098b7f6db97207b9ce111bac5ba7f0b0f
jfbus <jf@bustarret.com>
2018-07-11 11:16:23 +0200

    typo

--- time_test.go
+++ time_test.go
@@ -9,6 +9,6 @@ func TestTime(t *testing.T) {
 	t0 := time.Date(2018, 07, 11, 0, 0, 0, 100000000, time.UTC)
 	st := Time(t0)
 	if !GoTime(st).Equal(t0) {
-		t.Errorf("Time(GoTime) does not return the right tume")
+		t.Errorf("Time(GoTime) does not return the right time")
 	}
 }
